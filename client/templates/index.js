5af7e9ff417f7c31af2f2196927100a13af24318
ParkDongJo <pdj10211@gmail.com>
2019-05-26 20:03:00 +0900

    change direct structure of detail page

--- client/templates/index.js
+++ client/templates/index.js
@@ -8,6 +8,8 @@ import './asset/assetList.html';
 import './asset/assetList.js';
 import './asset/assetItem.html';
 import './asset/assetItem.js';
+import './asset/detail/assetDetail.html'
+import './asset/detail/assetDetail.js'
 
 import './funding/fundingPage.html';
 import './funding/fundingPage.js';
@@ -15,15 +17,12 @@ import './funding/fundingList.html';
 import './funding/fundingList.js';
 import './funding/fundItem.html';
 import './funding/fundItem.js';
-
-import './detail/asset/assetDetail.html'
-import './detail/asset/assetDetail.js'
-import './detail/funding/fundDetailPage.html'
-import './detail/funding/fundDetailPage.js'
-import './detail/funding/fundDetail.html'
-import './detail/funding/fundDetail.js'
-import './detail/funding/commentList.html'
-import './detail/funding/commentList.js'
+import './funding/detail/fundDetailPage.html'
+import './funding/detail/fundDetailPage.js'
+import './funding/detail/fundDetail.html'
+import './funding/detail/fundDetail.js'
+import './funding/detail/commentList.html'
+import './funding/detail/commentList.js'
 
 import './myPage/mypage.html';
 import './myPage/mypage.js';
