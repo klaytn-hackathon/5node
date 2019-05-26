381346c73ac66284dc7ab8f2584d702f6d97bab8
박승현 <ppillip@gmail.com>
2019-05-26 15:34:11 +0900

    수정합니다. :)

--- client/templates/login/KlaytnLoginButton.js
+++ client/templates/login/KlaytnLoginButton.js
@@ -1,41 +1,113 @@
-<template name="KlaytnLoginButton">
+/*
 
-    {{#if currentUser}}
-        <div>
-            {{currentUserName}} | <a class="KlaytnLogOutButton">Log Out▾</a>
-        </div>
-    {{else if loggingIn}}
-        로그인중...
-    {{else}}
-        <div>
-            <a class="KlaytnLoginButton">Klaytn Sign in ▾</a>
-        </div>
+   meteor npm install --save caver-js
 
-        {{#if signinMode}}
-            <div>
-                <p><input type="text" name="email" placeholder="메일"></p>
-                <p><input type="text" name="name" placeholder="이름"></p>
-                <p><input type="text" disabled name="password" value="{{pk}}" placeholder="비밀키">
-                    <input type="button" name="createPrivateKey" value="비밀키생성">
-                    <input type="button" name="copyPrivateKey" value="복사">
-                </p>
-                <p>비밀키 생성 후 안전한 곳에 저장하세요. <br/>분실하면 절대 찾을 수 없습니다.</p>
-                <p><button name="signUp">가입</button>
-                    <button name="cancel">취소</button></p>
-                <p><div class="goLogin">[로그인하기]</div></p>
-            </div>
-        {{/if}}
-        {{#if loginMode}}
-            <div>
-                <p><input type="text" name="loginEmail" placeholder="메일"></p>
-                <p><input type="text" name="loginPassword" placeholder="비밀키"></p>
-                <p><button name="login">로그인</button>
-                    <button name="cancel">취소</button></p>
-                <p><div class="goSignin">[가입하기]</div></p>
-            </div>
-        {{/if}}
+* */
 
+import {Template} from "meteor/templating";
+import {ReactiveVar} from "meteor/reactive-var";
+import Caver from 'caver-js';
 
-    {{/if}}
+import "./KlaytnLoginButton.html";
 
-</template>
+Template.KlaytnLoginButton.helpers({
+    pk() {
+        return Template.instance().privateKey.get();
+    },
+    signinMode(){
+        return Template.instance().mode.get()=="signin";
+    },
+    loginMode(){
+        return Template.instance().mode.get()=="login";
+    },
+    currentUserName(){
+        return Meteor.user().profile.name;
+    }
+});
+
+Template.KlaytnLoginButton.onCreated(function klaytnLoginOnCreated() {
+    this.caver = new Caver({rpcURL : 'http://api.baobab.klaytn.net:8651'});
+    this.privateKey = new ReactiveVar("");
+    this.klaytnAddress = "";
+    this.mode = new ReactiveVar(""); // login | signin
+});
+
+Template.KlaytnLoginButton.events({
+    "click .goLogin" (evt,tmpl){ tmpl.mode.set("login"); }
+    ,
+    "click .goSignin" (evt,tmpl){ tmpl.mode.set("signin"); }
+    ,
+    "click .KlaytnLogOutButton" (evt,tmpl){
+        Meteor.logout();
+    }
+    ,
+    "click .KlaytnLoginButton" (evt,tmpl){
+        if(tmpl.mode.get()==""){
+            return tmpl.mode.set("login");
+        }else{
+            return tmpl.mode.set("");
+        }
+    }
+    ,
+    "click input[name=createPrivateKey]" (evt,tmpl){
+        let accounts = tmpl.caver.klay.accounts.create();
+        tmpl.privateKey.set(accounts.privateKey);
+        tmpl.klaytnAddress = accounts.address;
+    }    ,
+    "click input[name=copyPrivateKey]" (evt,tmpl){
+        tmpl.find('input[name=password]').select();
+        document.execCommand("Copy");
+        alert("클립보드에 복사되었습니다.");
+    }
+    ,
+    "click button[name=signUp]" (evt,tmpl){
+        var email    = tmpl.find('input[name=email]').value;
+        var password = tmpl.find('input[name=password]').value;
+        var name     = tmpl.find('input[name=name]').value;
+        if(!password) {
+            alert("패스워드를 확인하세요");
+            return;
+        }
+
+        var klaytnAddress = tmpl.klaytnAddress;
+
+        var userInfo = { email, password, profile : { name ,klaytnAddress} };
+
+        Accounts.createUser(userInfo,function(error){
+            if(!!error){
+                alert(error.reason);
+            }else{
+                alert("가입 성공");
+                $(tmpl.findAll('input')).val("");
+
+                Meteor.loginWithPassword(email,password,function(error){
+                    if(!error){
+                        Meteor.logoutOtherClients();
+                        //todo 클레이튼연결하기
+                    }else{
+                        alert(error.reason);
+                    }
+                });
+            }
+        });
+    }
+    ,
+    "click button[name=login]" (evt,tmpl){
+
+        ttt = tmpl;
+        var email    = tmpl.find('input[name=loginEmail]').value;
+        var password = tmpl.find('input[name=loginPassword]').value;
+
+        Meteor.loginWithPassword(email,password,function(error){
+            if(!error){
+                Meteor.logoutOtherClients();
+                $(tmpl.findAll('input')).val("");
+
+                //todo 클레이튼연결하기
+
+            }else{
+                alert(error.reason);
+            }
+        });
+    }
+});
