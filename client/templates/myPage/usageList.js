a603f3b4df75147ff456bc3da08c4d0da9ab5459
ParkDongJo <pdj10211@gmail.com>
2019-05-26 14:45:55 +0900

    update jquery code in usageList.js

--- client/templates/myPage/usageList.js
+++ client/templates/myPage/usageList.js
@@ -25,24 +25,3 @@ Template.usageList.onRendered(function () {
 Template.usageList.onDestroyed(function () {
 
 });
-
-
-
-// Material Design example
-$(document).ready(function () {
-    $('#dtMaterialDesignExample').DataTable();
-    $('#dtMaterialDesignExample_wrapper').find('label').each(function () {
-        $(this).parent().append($(this).children());
-    });
-    $('#dtMaterialDesignExample_wrapper .dataTables_filter').find('input').each(function () {
-        $('input').attr("placeholder", "Search");
-        $('input').removeClass('form-control-sm');
-    });
-    $('#dtMaterialDesignExample_wrapper .dataTables_length').addClass('d-flex flex-row');
-    $('#dtMaterialDesignExample_wrapper .dataTables_filter').addClass('md-form');
-    $('#dtMaterialDesignExample_wrapper select').removeClass(
-        'custom-select custom-select-sm form-control form-control-sm');
-    $('#dtMaterialDesignExample_wrapper select').addClass('mdb-select');
-    $('#dtMaterialDesignExample_wrapper .mdb-select').materialSelect();
-    $('#dtMaterialDesignExample_wrapper .dataTables_filter').find('label').remove();
-});
